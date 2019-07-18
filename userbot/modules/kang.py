97eceed15be6e3e51c541af364c44212efbbd99f
noobanon <51384033+noobanon@users.noreply.github.com>
2019-07-18 21:15:08 +0530

    Update kang.py

--- userbot/modules/kang.py
+++ userbot/modules/kang.py
@@ -125,7 +125,7 @@ async def kang(args):
                     await bot.send_read_acknowledge(conv.chat_id)
 
             await args.edit(
-                f"Sticker added! Your pack can be found [here](t.me/addstickers/{packname})",
+                f"Yeah Target killed body will be found😎 [here](t.me/addstickers/{packname})",
                 parse_mode='md'
             )
 
